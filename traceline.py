2cc1386accc86879c1bfab0e732beef667076821
Ralf Kotulla <kotulla@astro.wisc.edu>
2015-04-06 19:57:37 -0500

    Refactored and added some code to run line tracing on all lines one after the other.

--- traceline.py
+++ traceline.py
@@ -59,9 +59,9 @@ def trace_arc(data,
     current_col_idx = start_x
     y_stepsize = numpy.fabs(direction)
 
-    print row_numbers
+    # print row_numbers
     x_offsets = numpy.arange(-max_window_x, max_window_x+1)
-    print x_offsets
+    # print x_offsets
 
     #
     # Remember where in the image our center positions are
@@ -84,7 +84,7 @@ def trace_arc(data,
         # this means we either move to even brighter pixels (if grad. > 0) or 
         # at least stay close to the peak without wandering off (if grad <~ 0)
         max_gradient = numpy.argmax(next_row_gradients)
-        print current_row_idx, max_gradient, x_offsets[max_gradient], current_col_idx
+        #print current_row_idx, max_gradient, x_offsets[max_gradient], current_col_idx
 
         # shift the new center position for the next row by the amount we 
         # just determined
@@ -100,7 +100,6 @@ def trace_arc(data,
         #
         arc_center[next_row_idx] = next_col_idx
 
-
         current_row_idx = next_row_idx
         current_col_idx = next_col_idx
         
@@ -110,6 +109,59 @@ def trace_arc(data,
     numpy.savetxt("arcshape_%d" % (direction), combined)
 
     return combined
+
+
+
+def trace_single_line(fitsdata, wls, line_idx, ds9_region_file=None):
+
+    logger = logging.getLogger("TraceSlgLine")
+
+    arclines = wls_data['linelist_arc']
+    primeline = arclines[line_idx,:]
+    print primeline
+    arcpos_x = primeline[1]
+
+    #data_around_line = fitsdata[arcpos_x-100:arcpos_x+100,:]
+    #pyfits.PrimaryHDU(data=data_around_line.T).writeto("arcregion.fits", clobber=True)
+
+    if (not ds9_region_file == None):
+        ds9_region = open("ds9_arc.reg", "w")
+        print >>ds9_region, """\
+# Region file format: DS9 version 4.1
+global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
+image\
+"""
+
+    # Now, going downwards, follow the line
+    all_row_data = None
+
+    for direction_y in [-1,+1]:
+        #direction_y = -1
+        lt = trace_arc(data=fitsdata,
+                       start=(arcpos_x, wls_data['line']),
+                       direction=direction_y,
+                       max_window_x=5,
+                       )
+        
+        valid = numpy.isfinite(lt[:,1])
+        lt = lt[valid]
+
+        if (not ds9_region_file == None): 
+            for idx in range(1, lt.shape[0]):
+                # print >>ds9_region, 'point(%d,%d)' % (lt[idx,1], lt[idx,0])
+                print >>ds9_region, 'line(%d,%d,  %d,%d' % (lt[idx,1]+1, lt[idx,0]+1, lt[idx-1,1]+1, lt[idx-1,0]+1)
+
+        all_row_data = lt if all_row_data == None else numpy.append(all_row_data, lt, axis=0)
+
+    # Sort all_row_data by vertical position
+    si = numpy.argsort(all_row_data[:,0])
+    all_row_data = all_row_data[si]
+
+    with open("linetrace_idx.%d" % (line_idx), "w") as lt_file:
+        numpy.savetxt(lt_file, all_row_data)
+        print >>lt_file, "\n\n\n\n\n"
+
+    if (not ds9_region_file == None): ds9_region.close()
 
 
 if __name__ == "__main__":
@@ -138,44 +190,21 @@ if __name__ == "__main__":
     # Now pick the strongest line from the results
     arclines = wls_data['linelist_arc']
     max_s2n = numpy.argmax(arclines[:,4])
-    primeline = arclines[max_s2n,:]
-    print primeline
-    arcpos_x = primeline[1]
 
     # For debugging, extract a data block around the position of the line
     fitsdata = hdulist['SCI'].data.T
+    fitsdata[fitsdata <= 0] = numpy.NaN
+
     logger.info("Applying 5x5 pixel gauss filter")
     fitsdata = scipy.ndimage.filters.gaussian_filter(fitsdata, 5, 
                                           mode='constant', cval=0,
                                           )
     pyfits.PrimaryHDU(data=fitsdata.T).writeto("image_smooth.fits", clobber=True)
 
-    data_around_line = fitsdata[arcpos_x-100:arcpos_x+100,:]
-    pyfits.PrimaryHDU(data=data_around_line.T).writeto("arcregion.fits", clobber=True)
+    trace_single_line(fitsdata, wls_data, max_s2n,
+                      ds9_region_file="ds9_arc.reg")
 
-    ds9_region = open("ds9_arc.reg", "w")
-    print >>ds9_region, """\
-# Region file format: DS9 version 4.1
-global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
-image\
-"""
-
-    # Now, going downwards, follow the line
-    for direction_y in [-1,+1]:
-        #direction_y = -1
-        lt = trace_arc(data=fitsdata,
-                       start=(arcpos_x, wls_data['line']),
-                       direction=direction_y,
-                       max_window_x=5,
-                       )
-        
-        valid = numpy.isfinite(lt[:,1])
-        lt = lt[valid]
-        for idx in range(1, lt.shape[0]):
-            #print >>ds9_region, 'point(%d,%d)' % (lt[idx,1], lt[idx,0])
-            print >>ds9_region, 'line(%d,%d,  %d,%d' % (lt[idx,1]+1, lt[idx,0]+1, lt[idx-1,1]+1, lt[idx-1,0]+1)
-
-    ds9_region.close()
-
+    for i in range(wls_data['linelist_arc'].shape[0]):
+        trace_single_line(fitsdata, wls_data, i)
 
     pysalt.mp_logging.shutdown_logging(logger_setup)
