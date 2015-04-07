24106195b7c5116e219bd950fe872c111a86ba35
Ralf Kotulla <kotulla@astro.wisc.edu>
2015-04-07 17:27:22 -0500

    Added some code to detect edges based on local curvature compared to curvature in the recent past. Seems to work, but not help a lot. On the other hand, only selecting few of the strongest lines seems to help in avoiding lines suddenly merging after they get too close together.

--- traceline.py
+++ traceline.py
@@ -34,6 +34,7 @@ def trace_arc(data,
               start,
               direction=-1, # -1: downwards, +1: upwards
               max_window_x=5, # how far do we allow the arc to move from one row to the row
+              max_corner_angle=30, # in degrees
               ):
 
     logger = logging.getLogger("TraceArc")
@@ -66,17 +67,29 @@ def trace_arc(data,
     #
     # Remember where in the image our center positions are
     #
-    arc_center = numpy.empty((data.shape[0]))
-    arc_center[:] = numpy.NaN
-    arc_center[start_y] = start_x
-    for next_row_idx in row_numbers:
-        logger.debug("Moving from row %4d to %4d" % (current_row_idx, next_row_idx))
+    arc_center = numpy.empty((data.shape[0],3))
+    arc_center[:,0] = numpy.NaN
+    arc_center[start_y,0] = start_x
+
+    n_pixels_for_corner = 3
+    for lines_since_start, next_row_idx in enumerate(row_numbers):
+        #logger.debug("Moving from row %4d to %4d" % (current_row_idx, next_row_idx))
 
         # extract a bunch of pixels in the next row around the position of the 
         # current peak
+
+        # Keep track of where the center position is
+
         next_row = data[current_col_idx-max_window_x:current_col_idx+max_window_x+1,
                         next_row_idx]
-        
+
+        # If next row contains pixels marked as NaN's stop work to avoid going 
+        # off into no-mans-land
+        if (numpy.sum(numpy.isnan(next_row) > 0)):
+            logger.info("Found illegal pixel in next row (%d, %d), stopping here!" % (
+                    current_col_idx, next_row_idx))
+            break
+
         # Now compute gradients
         next_row_gradients = next_row / y_stepsize
 
@@ -90,7 +103,74 @@ def trace_arc(data,
         # just determined
         next_col_idx = current_col_idx + x_offsets[max_gradient]
         
-                        
+        #
+        # Add some edge-detection here:
+        # If the direction of the arc changes by more than 30 degrees from the 
+        # direction across the past 10 pixels, assume this is an edge and stop 
+        # tracing the line at this point
+        #
+        #print current_row_idx
+        if (lines_since_start > 2*n_pixels_for_corner):
+            # compute the arc angles in the past N pixels and the N pixels 
+            # before that.
+            # If the two angles differ a lot ( >= max_corner_angle ) we found a 
+            # corner and can stop following the arc
+            dx_past = arc_center[row_numbers[lines_since_start - 2*n_pixels_for_corner],0] \
+                - arc_center[row_numbers[lines_since_start - 1*n_pixels_for_corner],0]
+            dx_now = arc_center[row_numbers[lines_since_start - 1*n_pixels_for_corner],0] \
+                - arc_center[row_numbers[lines_since_start-1],0]
+            # dx_past = arc_center[row_numbers[lines_since_start - 2*n_pixels_for_corner],0] \
+            #     - arc_center[current_row_idx - 1*n_pixels_for_corner,0]
+            # dx_now = arc_center[current_row_idx - 1*n_pixels_for_corner,0] \
+            #     - arc_center[current_row_idx - 0*n_pixels_for_corner,0]
+
+            angle_past = numpy.degrees(numpy.arctan2(dx_past, n_pixels_for_corner))
+            angle_now = numpy.degrees(numpy.arctan2(dx_now, n_pixels_for_corner))
+            #print current_row_idx, dx_past, dx_now, angle_past, angle_now 
+
+            arc_center[next_row_idx,1] = dx_now #angle_now
+            arc_center[next_row_idx,2] = dx_past #angle_past
+
+            if (math.fabs(angle_past - angle_now) > max_corner_angle):
+                # We found a corner
+                # --> retroactively stop following the line at the point the line 
+                #     started to deviate
+                logger.info("Corner detected in line %d: %.1f then vs %.1f now" % (
+                        current_row_idx, angle_past, angle_now))
+                avg_past = dx_past / n_pixels_for_corner
+                # for i in range(2*n_pixels_for_corners):
+                break
+
+        # corner_detected = False
+        # idx_n_pixels_back = 
+        # if (idx_n_pixels_back < 0 or idx_n_pixels_back > data.shape[1]):
+        #     # this would put us out of range, so no check here
+        #     corner_detected = False
+        #     logger.info("(line %4d) No check for angle <-- out of range (%d)" % (current_row_idx, idx_n_pixels_back))
+        # elif (lines_since_start < n_pixels_for_corner): #numpy.isnan(arc_center[idx_n_pixels_back,0])):
+        #     # again this is a bad pixels, most likely because we are not yet 
+        #     # <n_pixels_for_corner> pixels into the line
+        #     corner_detected = False
+        #     logger.info("(line %4d) No check for angle <-- too early" % (current_row_idx))
+        # else:
+        #     past_n_pixels = arc_center[idx_n_pixels_back,0] - current_col_idx
+        #     past_angle = math.degrees(numpy.arctan2(past_n_pixels, n_pixels_for_corner*direction))
+        #     now_angle = math.degrees(numpy.arctan2(x_offsets[max_gradient], direction))
+
+        #     arc_center[next_row_idx,1] = now_angle
+        #     arc_center[next_row_idx,2] = past_angle
+        #     if (math.fabs(past_angle - now_angle) > max_corner_angle):
+        #         logger.info("Identified corner at this point (%f vs %f), aborting!" % (
+        #                 now_angle, past_angle))
+        #         corner_detected = False
+                    
+        #     print current_row_idx, idx_n_pixels_back, past_n_pixels, past_angle, x_offsets[max_gradient], now_angle, corner_detected
+        #     # logger.info("(line %4d) Corner detection: shift(10px)=%3d --> angle=%7.1f   || now: %3d, angle=%7.1f ==> %5s" % (
+        #     #         current_row_idx, past_n_pixels, past_angle, x_offsets[max_gradient], now_angle, corner_detected))
+        # if (corner_detected):
+        #     break
+
+    
         if (math.fabs(next_row_idx-start_y) > 5000):
             logger.info("Aborting search!")
             break
@@ -98,14 +178,15 @@ def trace_arc(data,
         #
         # Save all positions for the next iteration
         #
-        arc_center[next_row_idx] = next_col_idx
-
+        arc_center[next_row_idx,0] = current_col_idx
         current_row_idx = next_row_idx
         current_col_idx = next_col_idx
         
     # For debugging: save the arc position
+    # combined = numpy.append(numpy.arange(data.shape[0]).reshape((-1,1)),
+    #                         arc_center.reshape((-1,1)), axis=1)
     combined = numpy.append(numpy.arange(data.shape[0]).reshape((-1,1)),
-                            arc_center.reshape((-1,1)), axis=1)
+                            arc_center, axis=1)
     numpy.savetxt("arcshape_%d" % (direction), combined)
 
     return combined
@@ -125,7 +206,7 @@ def trace_single_line(fitsdata, wls, line_idx, ds9_region_file=None):
     #pyfits.PrimaryHDU(data=data_around_line.T).writeto("arcregion.fits", clobber=True)
 
     if (not ds9_region_file == None):
-        ds9_region = open("ds9_arc.reg", "w")
+        ds9_region = open(ds9_region_file, "a")
         print >>ds9_region, """\
 # Region file format: DS9 version 4.1
 global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
@@ -147,6 +228,8 @@ image\
         lt = lt[valid]
 
         if (not ds9_region_file == None): 
+            print >>ds9_region, '# text(%d,%d) text={%d}' % (lt[0,1]+1, lt[0,0]+1, line_idx)
+
             for idx in range(1, lt.shape[0]):
                 # print >>ds9_region, 'point(%d,%d)' % (lt[idx,1], lt[idx,0])
                 print >>ds9_region, 'line(%d,%d,  %d,%d' % (lt[idx,1]+1, lt[idx,0]+1, lt[idx-1,1]+1, lt[idx-1,0]+1)
@@ -193,18 +276,37 @@ if __name__ == "__main__":
 
     # For debugging, extract a data block around the position of the line
     fitsdata = hdulist['SCI'].data.T
-    fitsdata[fitsdata <= 0] = numpy.NaN
+    #fitsdata[fitsdata <= 0] = numpy.NaN
 
     logger.info("Applying 5x0 pixel gauss filter")
-    fitsdata = scipy.ndimage.filters.gaussian_filter(fitsdata, (5,0), 
+    fitsdata_gf = scipy.ndimage.filters.gaussian_filter(fitsdata, (5,0), 
                                           mode='constant', cval=0,
                                           )
+    fitsdata_gf[fitsdata <= 0] = numpy.NaN
+    fitsdata = fitsdata_gf
+
     pyfits.PrimaryHDU(data=fitsdata.T).writeto("image_smooth.fits", clobber=True)
 
-    trace_single_line(fitsdata, wls_data, max_s2n,
-                      ds9_region_file="ds9_arc.reg")
+    # trace_single_line(fitsdata, wls_data, 21,
+    #                   ds9_region_file="ds9_arc.reg")
 
-    for i in range(wls_data['linelist_arc'].shape[0]):
-        trace_single_line(fitsdata, wls_data, i)
+    # Determine curvature with the 10 strongest lines
+    # Also eliminate all lines with nearby companions that might cause problems
+    sort_sn = numpy.argsort(wls_data['linelist_arc'][:,4])[::-1]
+    print wls_data['linelist_arc'][sort_sn][:10,4]
+    for i in range(15):
+        trace_single_line(fitsdata, wls_data, sort_sn[i],
+                           ds9_region_file="ds9_arc.reg")
+          
+    # trace_single_line(fitsdata, wls_data, max_s2n,
+    #                   ds9_region_file="ds9_arc.reg")
+
+    # for i in range(wls_data['linelist_arc'].shape[0]):
+    #     #skip line if S/N is too low
+    #     if (wls_data['linelist_arc'][i,4] < 50):
+    #         continue
+
+    #     trace_single_line(fitsdata, wls_data, i,
+    #                       ds9_region_file="ds9_arc.reg")
 
     pysalt.mp_logging.shutdown_logging(logger_setup)
