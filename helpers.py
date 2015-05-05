4063e2a012ca4ba76d338e232a60580ed6f9e146
Ralf Kotulla <kotulla@astro.wisc.edu>
2015-05-05 09:58:06 -0500

    refactored most of the functionality for the 2-d sky subtraction into a separate function that we can call from elsewhere.

---
+++ helpers.py
@@ -0,0 +1,187 @@
+
+
+import os, sys, glob, shutil
+
+import numpy
+import pyfits
+from scipy.ndimage.filters import median_filter
+import bottleneck
+import scipy.interpolate
+numpy.seterr(divide='ignore', invalid='ignore')
+
+# Disable nasty and useless RankWarning when spline fitting
+import warnings
+warnings.simplefilter('ignore', numpy.RankWarning)
+
+
+import pyfits
+import pysalt.mp_logging
+import logging
+import numpy
+
+
+
+
+
+
+#################################################################################
+#################################################################################
+#################################################################################
+def get_integrated_spectrum(hdu_rect, filename):
+    """
+
+    This function integrates the spectrum along the spectral axis, returning a 
+    1-D array of integrated intensities. 
+
+    """
+    
+    _,fb = os.path.split(filename)
+    logger = logging.getLogger("GetIntegratedSpec(%s)" % (fb))
+
+    integrated_intensity = bottleneck.nansum(hdu_rect['SCI'].data.astype(numpy.float32), axis=1)
+    logger.info("Integrated intensity: covers %d pixels along slit" % (integrated_intensity.shape[0]))
+    # pyfits.PrimaryHDU(data=integrated_intensity).writeto()
+    numpy.savetxt("1d_%s.cat" % (fb[:-5]), integrated_intensity)
+
+    return integrated_intensity
+
+
+
+#################################################################################
+#################################################################################
+#################################################################################
+def find_slit_profile(hdulist, filename, source_region=[1400,2600]):
+    """
+
+    Starting with the intensity profile along the slit, reject all likely 
+    sources (bright things), small-scale fluctuations (more sources, cosmics, 
+    etc), and finally produce a spline-smoothed profile of intensity along the
+    slit. This can then be used to correct the image data with the goal to 
+    improve sky-line subtraction.
+
+    """
+
+
+    _,fb = os.path.split(filename)
+    logger = logging.getLogger("FindSlitProf")
+
+    #
+    # Get binning information
+    #
+    binx, biny = pysalt.get_binning(hdulist)
+    if (binx == None):
+        logger.error("Can't find binning information. Does header CCDSUM exist?")
+        return None
+
+    #
+    # Get integrated slit profile
+    #
+    integrated_intensity = bottleneck.nansum(hdulist['SCI'].data.astype(numpy.float32), axis=1)
+    logger.info("Integrated intensity: covers %d pixels along slit" % (integrated_intensity.shape[0]))
+    # pyfits.PrimaryHDU(data=integrated_intensity).writeto()
+    numpy.savetxt("1d_%s.cat" % (fb[:-5]), integrated_intensity)
+
+    #
+    # First of all, reject all pixels with zero fluxes
+    #
+    bad_pixels = (integrated_intensity <= 0) | (numpy.isnan(integrated_intensity))
+
+    # Next, find average level across the profile.
+    # That way, we hopefully can reject all bright targets
+    bright_lim, faint_lim = 1e9, 0
+    # background = (integrated_intensity <= bright_lim) | \
+    #              (integrated_intensity >= faint_lim)
+    background = ~bad_pixels
+    likely_background_profile = numpy.array(integrated_intensity)
+    likely_background_profile[~background] = numpy.NaN
+    numpy.savetxt("1d_bg_%s.cat.start" % (fb[:-5]), likely_background_profile)
+
+    for i in range(5):
+        logger.info("Iteration %d: %d valid pixels considered BG" % (i+1, numpy.sum(background)))
+
+        # compute median of all pixels considered background
+        med = bottleneck.nanmedian(integrated_intensity[background])
+        std = bottleneck.nanstd(integrated_intensity[background])
+        logger.info("Med/Std: %f   %f" % (med, std))
+        # Now set new bright and faint limits
+        bright_lim, faint_lim = med+3*std, med-3*std
+
+        # Apply new mask to intensities
+        background = background & \
+                     (integrated_intensity <= bright_lim) & \
+                     (integrated_intensity >= faint_lim)
+
+        likely_background_profile = numpy.array(integrated_intensity)
+        likely_background_profile[~background] = numpy.NaN
+        numpy.savetxt("1d_bg_%s.cat.%d" % (fb[:-5], i+1), likely_background_profile)
+
+    skymask = ~bad_pixels & background
+
+    #
+    # Reject small outliers by median-filtering across a number of pixels
+    #
+    half_window_size = 25
+    
+    filtered_bg = numpy.array([
+        bottleneck.nanmedian(
+            likely_background_profile[i-half_window_size:i+half_window_size]) 
+        for i in range(likely_background_profile.shape[0])])
+    numpy.savetxt("1d_bg_%s.cat.filtered" % (fb[:-5]), filtered_bg)
+
+    #
+    # To smooth things out even better, fit a very low order spline, 
+    # avoiding the central area where the source likely is located
+    #
+    # Use 50 basepoints for the fitting polynomial
+    x = numpy.arange(filtered_bg.shape[0])
+    print 400/biny, 3600/biny, 100/biny
+
+    if (not source_region == None):
+        print "X:", filtered_bg.shape
+        do_not_fit = ((x<1400/biny) | (x>2600/biny)) & skymask
+
+        t = numpy.linspace(numpy.min(x[do_not_fit])+1, numpy.max(x[do_not_fit])-1, 50) #100/biny)
+        avoidance = (t>source_region[0]/biny) & (t<source_region[1]/biny)
+        t = t[~avoidance]
+        print t
+    else:
+        do_not_fit = skymask
+        t = numpy.linspace(numpy.min(x)+1, numpy.max(x)-1, 50)
+        
+    print "Using spline fitting base points\n",t
+
+    w = numpy.ones(filtered_bg.shape[0])
+    w[~skymask] = 0
+    numpy.savetxt("slitprofile.weights", w)
+    numpy.savetxt("slitprofile.do_not_fit", do_not_fit)
+    
+    print "xrange:",numpy.min(x[do_not_fit]), numpy.max(x[do_not_fit])
+    numpy.savetxt("1d_bg_%s.cat.basepoints" % (fb[:-5]), t, "%.2f")
+    numpy.savetxt("1d_bg_%s.cat.wt" % (fb[:-5]), w)
+    # lsq_spline = scipy.interpolate.LSQUnivariateSpline(
+    #     x=x[do_not_fit], y=filtered_bg[do_not_fit], t=t, 
+    #     w=None, bbox=[None, None], k=2)
+    lsq_spline = scipy.interpolate.LSQUnivariateSpline(
+        x=x, y=filtered_bg, t=t, 
+        w=w, bbox=[None, None], k=2)
+    numpy.savetxt("1d_bg_%s.cat.fit" % (fb[:-5]), lsq_spline(x))
+
+    #
+    # Also try fitting a polynomial to the function
+    #
+    #numpy.polyfit(
+
+    #
+    # Now normalize this line profile so we can use it to flatten out the slit image
+    #
+    avg_flux = bottleneck.nanmean(filtered_bg)
+    print "average slit across slit profile:", avg_flux
+
+    #slit_flattening = filtered_bg / avg_flux
+    slit_flattening = lsq_spline(x) / avg_flux
+    # Fill in gaps with ones
+    slit_flattening[numpy.isnan(slit_flattening)] = 1.0
+
+    return slit_flattening.reshape((-1,1)), skymask, filtered_bg.reshape((-1,1))/avg_flux
+
+
--- skysub2d.py
+++ skysub2d.py
@@ -149,6 +149,144 @@ def compute_spline_sky_spectrum(all_skies,
     return sky_spectrum_spline
 
 
+def make_2d_skyspectrum(hdulist, 
+                        wls_2d,
+                        sky_regions=None, 
+                        oversample_factor=2.0):
+    """
+    Compute a full 2-D sky spectrum, including curvature, based on the input 
+    HDUList and the 2-D wavelength solution created from an appropriate ARC 
+    spectrum using the same setup.
+
+    Parameters
+    ----------
+
+    hdulist : pyfits.HDUList
+
+        multi-extension FITS HDUList of input object frame. 
+
+    wls_2d : numpy 2d array
+
+        two-dimensional numpy array with wavelengths for each pixel
+
+    sky_regions : numpy (N,2) array
+
+        list of y-positions (from,to) marking which positions along the slit 
+        (vertical bands if image is displayed in ds9) to be used for extracting 
+        the sky spectrum.
+
+    oversample_factor : float
+
+        ratio between number of spline basepoints to be used for interpolating 
+        the sky spectrum and the number of pixels in spectral direction in the
+        input object frame.
+
+
+    Returns
+    -------
+
+    2-d sky spectrum as numpy ndarray.
+
+
+    """
+
+    logger = logging.getLogger("Make2DSkySpec")
+
+    #
+    # Now extract some sky-spectrum from the specified y-range 
+    #
+
+    obj_data = hdulist['SCI'].data
+
+    # Remember: Both FITS data __AND__ WLS_2D data are in [y,x] ordering
+    all_skies = None
+
+    obj_masked = numpy.empty(obj_data.shape)
+    obj_masked[:,:] = numpy.NaN
+
+    for idx, sky_region in enumerate(sky_regions):
+        logger.debug("Adding sky-region: y = %4d ... %4d" % (sky_region[0], sky_region[1]))
+
+        #print obj_data.shape,  sky_region[0], sky_region[1]
+        data_region = obj_data[sky_region[0]:sky_region[1], :]
+        wls_region = wls_2d[sky_region[0]:sky_region[1], :]
+        
+        obj_masked[sky_region[0]:sky_region[1], :] = obj_data[sky_region[0]:sky_region[1], :]
+
+        # Now merge data and wavelengths
+        # this gives us a 2-D array, shape N,2 with WL in the zero-th column, 
+        # and fluxes in the first
+        #print data_region.shape, wls_region.shape
+        data_wls = numpy.append(wls_region.reshape((-1,1)),
+                                data_region.reshape((-1,1)),
+                                axis=1)
+        # For now dump this data to file
+        # if (idx == 0):
+        #     numpy.savetxt("wl+data__%d-%d.dump" % (sky_region[0],sky_region[1]),
+        #                   data_wls)
+
+        all_skies = data_wls if all_skies == None else \
+            numpy.append(all_skies, data_wls, axis=0)
+
+    pyfits.HDUList([pyfits.PrimaryHDU(header=hdulist['SCI'].header,
+                                      data=obj_masked)]).writeto("obj_masked.fits", clobber=True)
+
+    #
+    # Exclude all points with NaNs in either wavelength or flux
+    #
+    good_pixel = numpy.isfinite(all_skies[:,0]) & numpy.isfinite(all_skies[:,1])
+    all_skies = all_skies[good_pixel]
+    
+    #
+    # also sort all pixels to be ascending in wavelength, otherwise the spline 
+    # fitting will crap out with some "Interior knots t must satisfy "
+    # Schoenberg-Whitney conditions" error message that does not seem to make 
+    # any sense
+    #
+    wl_sort = numpy.argsort(all_skies[:,0])
+    all_skies = all_skies[wl_sort]
+
+    numpy.savetxt("allskies", all_skies[::10])
+
+    ############################################################################
+    #
+    # Now we have a full list of wavelengths and presumed sky fluxes
+    #
+    ############################################################################
+
+
+    #
+    # Fit a spline to the spectrum. Use N times as many basepoints as there 
+    # are pixels in spectral direction in the original FITS data
+    #
+    #N_oversample = 1.1 #2.
+    N_original = obj_data.shape[1]
+    logger.info("Oversampling %d input pixels by a factor of %.1f" % (
+            N_original, oversample_factor))
+    n_basepoints = N_original * oversample_factor
+    sky_spectrum_spline = compute_spline_sky_spectrum(
+        all_skies, 
+        n_basepoints=n_basepoints,
+        N_min=10,
+        show_plot_range=[5800,6000],
+        )
+
+
+    #
+    # Now with the spline fit to the sky-spectrum, we can compute the 2-D sky 
+    # spectrum for the full input frame, including the curvature in the spectral 
+    # dimension which we haven't compensated for yet.
+    #
+    logger.info("Computing full-frame, 2-D sky spectrum, incl. curvature ...")
+    sky_2d = sky_spectrum_spline(wls_2d.ravel())
+    sky_2d = sky_2d.reshape(wls_2d.shape)
+    
+    # For now, write the sky spectrum to FITS so we can have a look at it in ds9
+    pyfits.HDUList([pyfits.PrimaryHDU(data=sky_2d)]).writeto("sky_2d.fits", clobber=True)
+
+    return sky_2d
+
+
 if __name__ == "__main__":
 
     logger_setup = pysalt.mp_logging.setup_logging()
@@ -181,101 +319,9 @@ if __name__ == "__main__":
             ])
     obj_out.writeto(sys.argv[3], clobber=True)
 
-    #
-    # Now extract some sky-spectrum from the specified y-range 
-    #
-
-    obj_data = obj_hdulist['SCI'].data
 
     user_sky = sys.argv[4]
     sky_regions = numpy.array([x.split(":") for x in user_sky.split(",")]).astype(numpy.int)
-
-    # Remember: Both FITS data __AND__ WLS_2D data are in [y,x] ordering
-    all_skies = None
-
-    obj_masked = numpy.empty(obj_data.shape)
-    obj_masked[:,:] = numpy.NaN
-
-    for idx, sky_region in enumerate(sky_regions):
-        print sky_region
-
-        print obj_data.shape,  sky_region[0], sky_region[1]
-        data_region = obj_data[sky_region[0]:sky_region[1], :]
-        wls_region = wls_2d[sky_region[0]:sky_region[1], :]
-        
-        obj_masked[sky_region[0]:sky_region[1], :] = obj_data[sky_region[0]:sky_region[1], :]
-
-        # Now merge data and wavelengths
-        # this gives us a 2-D array, shape N,2 with WL in the zero-th column, 
-        # and fluxes in the first
-        print data_region.shape, wls_region.shape
-        data_wls = numpy.append(wls_region.reshape((-1,1)),
-                                data_region.reshape((-1,1)),
-                                axis=1)
-        # For now dump this data to file
-        # if (idx == 0):
-        #     numpy.savetxt("wl+data__%d-%d.dump" % (sky_region[0],sky_region[1]),
-        #                   data_wls)
-
-        all_skies = data_wls if all_skies == None else \
-            numpy.append(all_skies, data_wls, axis=0)
-
-    pyfits.HDUList([pyfits.PrimaryHDU(header=obj_hdulist['SCI'].header,
-                                      data=obj_masked)]).writeto("obj_masked.fits", clobber=True)
-
-    #
-    # Exclude all points with NaNs in either wavelength or flux
-    #
-    good_pixel = numpy.isfinite(all_skies[:,0]) & numpy.isfinite(all_skies[:,1])
-    all_skies = all_skies[good_pixel]
-    
-    #
-    # also sort all pixels to be ascending in wavelength, otherwise the spline 
-    # fitting will crap out with some "Interior knots t must satisfy "
-    # Schoenberg-Whitney conditions" error message that does not seem to make 
-    # any sense
-    #
-    wl_sort = numpy.argsort(all_skies[:,0])
-    all_skies = all_skies[wl_sort]
-
-    numpy.savetxt("allskies", all_skies[::10])
-
-    ############################################################################
-    #
-    # Now we have a full list of wavelengths and presumed sky fluxes
-    #
-    ############################################################################
-
-
-    #
-    # Fit a spline to the spectrum. Use N times as many basepoints as there 
-    # are pixels in spectral direction in the original FITS data
-    #
-    N_oversample = 2.
-    N_original = obj_data.shape[1]
-    logger.info("Oversampling %d input pixels by a factor of %.1f" % (
-            N_original, N_oversample))
-    n_basepoints = N_original * N_oversample
-    sky_spectrum_spline = compute_spline_sky_spectrum(
-        all_skies, 
-        n_basepoints=n_basepoints,
-        N_min=10,
-        show_plot_range=[5800,6000],
-        )
-
-
-    #
-    # Now with the spline fit to the sky-spectrum, we can compute the 2-D sky 
-    # spectrum for the full input frame, including the curvature in the spectral 
-    # dimension which we haven't compensated for yet.
-    #
-    logger.info("Computing full-frame, 2-D sky spectrum, incl. curvature ...")
-    sky_2d = sky_spectrum_spline(wls_2d.ravel())
-    sky_2d = sky_2d.reshape(wls_2d.shape)
-    
-    # For now, write the sky spectrum to FITS so we can have a look at it in ds9
-    pyfits.HDUList([pyfits.PrimaryHDU(data=sky_2d)]).writeto("sky_2d.fits", clobber=True)
-
 
     #
     # Perform the sky-subtraction (this is now easy as pie)
