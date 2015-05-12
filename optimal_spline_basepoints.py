0904111cafccc09cef64ff8733ef07e381262020
Ralf Kotulla <kotulla@astro.wisc.edu>
2015-05-12 14:06:33 -0500

    Added code to read data directly from MEF FITS file rather than a ASCII txt file. This also allows to include the pixel variance to be included in the spline fit, and will hopefully allow to do proper outlier rejection to improve the overall fit quality.

--- optimal_spline_basepoints.py
+++ optimal_spline_basepoints.py
@@ -28,13 +28,37 @@ if __name__ == "__main__":
     logger = logging.getLogger("OptSplineKs")
 
 
-    allskies_filename = sys.argv[1]
+    obj_fitsfile = sys.argv[1]
+    obj_hdulist = pyfits.open(obj_fitsfile)
+
+    logger.info("Loading all data from FITS")
+    obj_data = obj_hdulist['SCIRAW'].data
+    obj_wl   = obj_hdulist['WAVELENGTH'].data
+    obj_rms  = obj_hdulist['VAR'].data
+    obj_bpm  = obj_hdulist['BPM'].data.flatten()
+
+    # now merge all data frames into a single 3-d numpy array
+    obj_cube = numpy.empty((obj_data.shape[0], obj_data.shape[1], 3))
+    obj_cube[:,:,0] = obj_wl[:,:]
+    obj_cube[:,:,1] = obj_data[:,:]
+    obj_cube[:,:,2] = obj_rms[:,:]
+
+    obj_cube = obj_cube.reshape((-1, 3))
+
+    # Now exclude all pixels marked as bad
+    obj_cube = obj_cube[obj_bpm == 0]
+
+    logger.info("%7d pixels left after eliminating bad pixels!" % (obj_cube.shape[0]))
+
+    allskies = obj_cube[::5]
+
+    
     
 
-    #
-    # Load and prepare data
-    #
-    allskies = numpy.loadtxt(allskies_filename)
+    # #
+    # # Load and prepare data
+    # #
+    # allskies = numpy.loadtxt(allskies_filename)
 
     # just to be on the safe side, sort allskies by wavelength
     sky_sort_wl = numpy.argsort(allskies[:,0])
@@ -126,6 +150,52 @@ if __name__ == "__main__":
                                              spline_opt(k_wl).reshape((-1,1)),
                                              axis=1)
                   )
+
+    
+    logger.info("Computing spline using optimized sampling and outlier rejection")
+    good_point = (allskies[:,0] > 0)
+    print good_point.shape
+    print good_point
+    for iteration in range(3):
+        
+        # compute spline
+        spline_iter = scipy.interpolate.LSQUnivariateSpline(
+            x=allskies[good_point,0],#[good_point], 
+            y=allskies[good_point,1],#[good_point], 
+            t=k_wl,
+            w=None, # no weights (for now)
+            bbox=[wl_min, wl_max], 
+            k=3, # use a cubic spline fit
+            )
+        numpy.savetxt("spline_opt.iter%d" % (iteration+1), 
+                      numpy.append(k_wl.reshape((-1,1)),
+                                   spline_opt(k_wl).reshape((-1,1)),
+                                   axis=1)
+                  )
+        
+        
+        # compute spline fit for each wavelength data point
+        dflux = allskies[:,1] - spline_iter(allskies[:,0])
+        print dflux
+
+        #
+        # Add here: work out the scatter of the distribution of pixels in the 
+        #           vicinity of this basepoint. This is what determined outlier 
+        #           or not, and NOT the uncertainty in a given pixel
+        #
+
+        # and check how significant this deviation is
+        dsigma = dflux / allskies[:,2]
+        print dsigma
+
+        
+        # now exclude all data points that deviate by more than 3 sigma
+        good_point = numpy.fabs(dsigma) < 3
+        print good_point.shape
+        print good_point
+
+        logger.info("Done with iteration %d (%d pixels left)" % (iteration+1, numpy.sum(good_point)))
+
 
     #
     # Compute differences between data and spline fit for both cases
